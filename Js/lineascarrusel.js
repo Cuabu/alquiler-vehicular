3f12e596dc9404e90c7e428df636d88de4f0083d
Cuabu <albertocuaburodriguez@gmail.com>
2026-07-04 18:11:55 -0500

    actualizando el repositorio

--- Js/lineascarrusel.js
+++
@@ -1,9 +0,0 @@
-function showText(imageNumber) {
-    const textElement = document.getElementById(`text${imageNumber}`);
-    textElement.style.display = "block";
-}
-
-function hideText(imageNumber) {
-    const textElement = document.getElementById(`text${imageNumber}`);
-    textElement.style.display = "none";
-}
